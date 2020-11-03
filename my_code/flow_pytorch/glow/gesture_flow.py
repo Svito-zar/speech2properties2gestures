Correct naming + fix sigma numerical issue

my_code/flow_pytorch/glow/gesture_flow.py
@@ -11,7 +11,7 @@ from pytorch_lightning import LightningModule
 from torch.optim import SGD, Adam, RMSprop
 from torch.optim.lr_scheduler import LambdaLR, MultiplicativeLR, StepLR
 from torch.utils.data import DataLoader
-from my_code.flow_pytorch.glow.modules import GaussianDiag, GeneralGaussian
+from my_code.flow_pytorch.glow.modules import StandardGaussian, DiagGaussian
 
 from my_code.flow_pytorch.data.trinity_taras import SpeechGestureDataset, inv_standardize
 
@@ -177,9 +177,9 @@ class GestureFlow(LightningModule):
 
             z_enc, objective = self.glow(x=curr_output, condition=curr_cond)
 
-            log_sigma = mean = torch.zeros_like(curr_output)
+            log_sigma = mu = torch.zeros_like(curr_output)
 
-            tmp_loss = self.loss(objective, z_enc, mean, log_sigma)
+            tmp_loss = self.loss(objective, z_enc, mu, log_sigma)
 
             losses.append(tmp_loss.cpu().detach())
             loss += torch.mean(tmp_loss)
@@ -188,10 +188,10 @@ class GestureFlow(LightningModule):
 
         return z_seq, (loss / len(z_seq)).unsqueeze(-1), losses
 
-    def loss(self, objective, z, mean, log_sigma):
-        log_likelihood = objective + GeneralGaussian.logp_sum(mean, log_sigma, z)
-        """vers_1 = GaussianDiag.logp_sum_simplified(z)
-        vers_2 = GeneralGaussian.logp_sum(mean, log_sigma, z)
+    def loss(self, objective, z, mu, log_sigma):
+        log_likelihood = objective + DiagGaussian.log_likelihood(mu, log_sigma, z)
+        """vers_1 = StandardGaussian.logp_sum_simplified(z)
+        vers_2 = DiagGaussian.log_likelihood(mu, log_sigma, z)
         print("Diag: ", vers_1[0])
         print("Non-Diag: ", vers_2[0])"""
         nll = (-log_likelihood) / float(np.log(2.0))
@@ -423,10 +423,10 @@ class GestureFlow(LightningModule):
             )
 
 
-            log_sigma = mean = torch.zeros_like(z_enc)
+            log_sigma = mu = torch.zeros_like(z_enc)
 
             backward_loss += torch.mean(
-                self.loss(-backward_objective, z_enc, mean, log_sigma)
+                self.loss(-backward_objective, z_enc, mu, log_sigma)
             )
 
             reconstr_seq.append(reconstr.detach())

my_code/flow_pytorch/glow/models.py
@@ -360,9 +360,9 @@ class Glow(nn.Module):
         with torch.no_grad():
             if z is None:
                 if mean is None:
-                    z = modules.GaussianDiag.sample_iid(output_shape, std).to(condition.device)
+                    z = modules.StandardGaussian.sample_iid(output_shape, std).to(condition.device)
                 else:
-                    z = modules.GeneralGaussian.sample(mean, std).to(condition.device)
+                    z = modules.DiagGaussian.sample(mean, std).to(condition.device)
             x, logdet = self.flow(z, condition, std=std, reverse=True)
         return x, logdet
 

my_code/flow_pytorch/glow/modules.py
@@ -194,7 +194,7 @@ class InvertibleConv1x1(nn.Module):
             return z, logdet
 
 
-class GaussianDiag:
+class StandardGaussian:
     Log2PI = float(np.log(2 * np.pi))
 
     @staticmethod
@@ -204,11 +204,11 @@ class GaussianDiag:
               k = 1 (Independent)
               Var = logs ** 2
         """
-        return -0.5 * ((x ** 2) + GaussianDiag.Log2PI)
+        return -0.5 * ((x ** 2) + StandardGaussian.Log2PI)
 
     @staticmethod
     def logp_sum_simplified(x):
-        likelihood = GaussianDiag.log_likelihood_simplified(x)
+        likelihood = StandardGaussian.log_likelihood_simplified(x)
         return torch.sum(likelihood, dim=1)
 
     @staticmethod
@@ -219,12 +219,12 @@ class GaussianDiag:
         )
 
 
-class GeneralGaussian:
+class DiagGaussian:
     Log2PI = float(np.log(2 * np.pi))
     eps = 1e-8
 
     @staticmethod
-    def log_likelihood(mean, log_sigma, x):
+    def log_likelihood(mu, log_sigma, x):
         """
         lnL = -  ln|sigma|  -1/2 * {((X - Mu)^T)(sigma^-2)(X - Mu) + kln(2*PI) }
         """
@@ -233,14 +233,13 @@ class GeneralGaussian:
         # sigma = eps + sigmoid(activation_out)
         # sigma = eps + softplus(activation_out, beta)
 
-        sigma = GeneralGaussian.eps + torch.exp(log_sigma)
+        sigma = DiagGaussian.eps + torch.exp(log_sigma)
+        log_sigma = torch.log(sigma)
 
-        return - log_sigma - 0.5 * (((x - mean) ** 2) / sigma + GaussianDiag.Log2PI)
+        likelihood = - log_sigma - 0.5 * (((x - mu) ** 2) / sigma + StandardGaussian.Log2PI)
 
-    @staticmethod
-    def logp_sum(mean, logs, x):
-        likelihood = GeneralGaussian.log_likelihood(mean, logs, x)
         return thops.sum(likelihood, dim=[1])
+
 
     @staticmethod
     def sample(mean, std):