Fixing logdet calculation Towards solving the issue https://github.com/Svito-zar/probabilistic-gesticulator/issues/3 Logdet is calculated separately for each time-step in the sequence and does not need any multiplicative factors anymore

my_code/flow_pytorch/glow/modules.py
@@ -57,9 +57,8 @@ class ActNorm2d(nn.Module):
         if logdet is not None:
             """
             logs is log_std of `mean of channels`
-            so we need to multiply on the channel length
             """
-            dlogdet = thops.sum(logs) * input.size(1)
+            dlogdet = thops.sum(logs)
             if reverse:
                 dlogdet *= -1
             logdet = logdet + dlogdet