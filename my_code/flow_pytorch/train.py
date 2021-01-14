5d000d62be8806a9547be41dd2c107ea5de60694
Taras Kucherenko <tarask@kth.se>
2021-01-14 11:31:01 +0100

    Enable Comet ML logging

--- my_code/flow_pytorch/train.py
+++ my_code/flow_pytorch/train.py
@@ -3,7 +3,6 @@ from my_code.flow_pytorch.glow.gesture_flow import GestureFlow
 from my_code.flow_pytorch.glow.utils import get_hparams
 from my_code.misc.shared import BASE_DIR, CONFIG, DATA_DIR, RANDOM_SEED
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning import loggers as pl_loggers
 
 seed_everything(RANDOM_SEED)
 
@@ -14,13 +13,24 @@ if __name__ == "__main__":
         hparams.data_root
     ), "Failed to find root dir `{}` of dataset.".format(hparams.dataset_root)
 
+    if hparams.comet_logger["api_key"] != "None":
+        from pytorch_lightning.loggers import CometLogger
+
+        logger = CometLogger(
+            api_key=hparams.comet_logger["api_key"],
+            project_name=hparams.comet_logger["project_name"],
+        )
+    else:
+        from pytorch_lightning import loggers as pl_loggers
+        logger = pl_loggers.TensorBoardLogger('lightning_logs/')
+
+
+
     hparams.num_dataloader_workers = 0
     hparams.gpus = 0
 
     model = GestureFlow(hparams)
 
-    tb_logger = pl_loggers.TensorBoardLogger('lightning_logs/')
-
-    trainer = Trainer.from_argparse_args(hparams, logger=tb_logger, profiler="simple") # profiler="advanced"
+    trainer = Trainer.from_argparse_args(hparams, logger=logger) #, profiler="simple") # profiler="advanced"
 
     trainer.fit(model)
