580aa275eee5d57ebff33ced913379662837c3e2
Taras Kucherenko <tarask@kth.se>
2021-02-03 11:45:30 +0100

    Clean the gesture annotation data

--- my_code/data_processing/annotations/investigate_data.py
+++ my_code/data_processing/annotations/investigate_data.py
@@ -7,18 +7,18 @@ curr_folder = "/home/tarask/Documents/Datasets/SaGa/All_the_transcripts/"
 curr_file = curr_folder + "/01_video_NEW.eaf"
 elan = pympi.Elan.Eaf(file_path=curr_file)
 
+
 gen_tiers = [ "R.G.Left.Phrase", "R.G.Left.Phase", "R.G.Right.Phrase", "R.G.Right.Phase",
-              "R.Movement_relative_to_other_Hand", "F.G.Right.Phrase", "F.G.Right.Phase",
-              "F.G.Left.Phrase", "F.G.Left.Phase", "R.S.Form", "R.S.Pos", "F.S.Form", "F.S.Pos",
+              "R.Movement_relative_to_other_Hand", "R.S.Form", "R.S.Pos",
               "R.G.Right Semantic", "R.S.Semantic Feature"]
+
 
 #time_key = elan.timeslots
 
 # go though the gesture features
 for column in gen_tiers:
 
-    curr_values_pre = []
-    curr_values_post = []
+    curr_values = []
 
     # go through all the files in the dataset
 
@@ -38,20 +38,41 @@ for column in gen_tiers:
 
         for key, (st_t, end_t, label, _) in curr_tier.items():
             if label is not None:
+                # remove leading whitespace
+                label = label.lstrip()
                 if label != "" and label != " ":
-                    if False: #label.find('-')!=-1:
+                    if label.find('-')!=-1:
                         split = re.split('-',label)
-                        curr_values_pre.append(pre)
-                        curr_values_post.append(post)
+                        for subval in split:
+                            curr_values.append(subval.strip())
+                    elif label.find('/')!=-1:
+                        split = re.split('/',label)
+                        for subval in split:
+                            curr_values.append(subval.strip())
+                    elif label.find(',')!=-1:
+                        split = re.split(',',label)
+                        for subval in split:
+                            curr_values.append(subval.strip())
+                    elif label.find('\n')!=-1:
+                        split = re.split('\n',label)
+                        for subval in split:
+                            curr_values.append(subval.strip())
+                            break
+                    elif label == "relative Positionm Amount":
+                        curr_values.append("relative Position")
+                        curr_values.append("Amount")
+                    elif label == "relatie Position":
+                        curr_values.append("relative Position")
+                    elif label == "Shape38":
+                        curr_values.append("Shape")
                     else:
-                        curr_values_pre.append(label)
+                        curr_values.append(label)
 
-    if len(set(curr_values_pre)) > 0:
-        print(column, len(set(curr_values_pre)))
-        unique_values = np.array(set(curr_values_pre))
+    if len(set(curr_values)) > 0:
+        print(column, len(set(curr_values)))
+        unique_values = np.array(set(curr_values))
         print(unique_values)
 
         print("\n")
-
 
 print("Done!")
