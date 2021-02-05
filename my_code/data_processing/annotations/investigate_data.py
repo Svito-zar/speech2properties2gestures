ecb45f7faf88120ce9d579d7d57ec4ba050ef99c
Taras Kucherenko <tarask@kth.se>
2021-02-05 08:56:57 +0100

    Enhanced data investigation script - add new properties as well

--- my_code/data_processing/annotations/investigate_data.py
+++ my_code/data_processing/annotations/investigate_data.py
@@ -7,13 +7,40 @@ curr_folder = "/home/tarask/Documents/Datasets/SaGa/All_the_transcripts/"
 curr_file = curr_folder + "/01_video_NEW.eaf"
 elan = pympi.Elan.Eaf(file_path=curr_file)
 
+# Detail
 
-gen_tiers = [ "R.G.Left.Phrase", "R.G.Left.Phase", "R.G.Right.Phrase", "R.G.Right.Phase",
+gen_tiers = ["R.G.Right.Practice", "R.G.Left.Phrase", "R.G.Left.Phase", "R.G.Right.Phrase", "R.G.Right.Phase",
               "R.Movement_relative_to_other_Hand", "R.S.Form", "R.S.Pos",
               "R.G.Right Semantic", "R.S.Semantic Feature"]
 
 
-#time_key = elan.timeslots
+def clean_label(label):
+    # remove leading whitespace
+    label = label.lstrip()
+
+    if label.find('-') != -1:
+        split = re.split('-', label)
+        label_cleaned = [subval.strip() for subval in split]
+    elif label.find('/') != -1:
+        split = re.split('/', label)
+        label_cleaned = [subval.strip() for subval in split]
+    elif label.find(',') != -1:
+        split = re.split(',', label)
+        label_cleaned = [subval.strip() for subval in split]
+    elif label.find('\n') != -1:
+        split = re.split('\n', label)
+        label_cleaned = [split[0].strip()]
+    elif label == "relative Positionm Amount":
+        label_cleaned = ["relative Position","Amount"]
+    elif label == "relatie Position":
+        label_cleaned =["relative Position"]
+    elif label == "Shape38":
+        label_cleaned = ["Shape"]
+    else:
+        label_cleaned = [label]
+
+    return label_cleaned
+
 
 # go though the gesture features
 for column in gen_tiers:
@@ -33,40 +60,25 @@ for column in gen_tiers:
         if column in curr_tiers:
             #print(item)
             curr_tier = curr_tiers[column][0]
+            if len(curr_tier) == 0:
+                curr_tier = curr_tiers[column][1]
         else:
             break
 
-        for key, (st_t, end_t, label, _) in curr_tier.items():
-            if label is not None:
-                # remove leading whitespace
-                label = label.lstrip()
-                if label != "" and label != " ":
-                    if label.find('-')!=-1:
-                        split = re.split('-',label)
-                        for subval in split:
-                            curr_values.append(subval.strip())
-                    elif label.find('/')!=-1:
-                        split = re.split('/',label)
-                        for subval in split:
-                            curr_values.append(subval.strip())
-                    elif label.find(',')!=-1:
-                        split = re.split(',',label)
-                        for subval in split:
-                            curr_values.append(subval.strip())
-                    elif label.find('\n')!=-1:
-                        split = re.split('\n',label)
-                        for subval in split:
-                            curr_values.append(subval.strip())
-                            break
-                    elif label == "relative Positionm Amount":
-                        curr_values.append("relative Position")
-                        curr_values.append("Amount")
-                    elif label == "relatie Position":
-                        curr_values.append("relative Position")
-                    elif label == "Shape38":
-                        curr_values.append("Shape")
-                    else:
-                        curr_values.append(label)
+        for key, value in curr_tier.items():
+            if len(value) == 4:
+                (st_t, end_t, label, _) = value
+                # sometime they are messed up
+                if label is None and _ is None:
+                    (st_t, label, _, _) = value
+            elif len(value) == 3:
+                (st_t, label, _) = value
+            elif len(value) == 2:
+                (st_t, label) = value
+            if label is not None and label != "" and label != " ":
+                label_cleaned = clean_label(label)
+                for label_parts in label_cleaned:
+                        curr_values.append(label_parts)
 
     if len(set(curr_values)) > 0:
         print(column, len(set(curr_values)))
@@ -74,5 +86,9 @@ for column in gen_tiers:
         print(unique_values)
 
         print("\n")
+
+    else:
+        print(column, " is empty ...")
+        print(curr_values)
 
 print("Done!")
