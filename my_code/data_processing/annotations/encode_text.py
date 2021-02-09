412dd347796cfd59e87d02010f8ae1c01805e79f
Taras Kucherenko <tarask@kth.se>
2021-02-09 13:25:59 +0100

    Text data cleaning + encoding using sub-words

--- my_code/data_processing/annotations/encode_text.py
+++ my_code/data_processing/annotations/encode_text.py
@@ -6,6 +6,33 @@ import os
 import h5py
 
 
+def clean_str(word):
+    """
+    Data cleaning for the text
+    Args:
+        word:         input word string
+
+    Returns:
+        final_word:   cleaned version of the word string
+
+    """
+
+    new_word = word.replace(" ", "").replace("s...", "so").replace("???", "").replace("-", "_")
+
+    special_chars = ".!@#$%^&*()_"
+
+    for sp_char in special_chars:
+        if sp_char in new_word:
+            new_word = new_word.replace(sp_char, "")
+
+    final_word = new_word.replace(" ", "")
+
+    if final_word == "":
+        final_word = "ah"
+
+    return final_word
+
+
 def text_to_feat(tokenizer, model, text):
     """
     Encode given text into BERT feature vector
@@ -18,14 +45,43 @@ def text_to_feat(tokenizer, model, text):
         text_enc     resulting feature vector
 
     """
-    input_ids = torch.tensor(tokenizer.encode(text)).unsqueeze(0)
+
+    # Clean text first
+    text_str = " ".join(clean_str(str(x)) for x in text)
+
+    # Encode the whole text split into sub-words
+    input_ids = torch.tensor(tokenizer.encode(text_str)).unsqueeze(0)
     outputs = model(input_ids)
     last_hidden_states = outputs[0]
-    text_enc = last_hidden_states[0, 1:-1]
+    sub_word_encodings = last_hidden_states[0, 1:-1]
+
+    actual_tokens = tokenizer.convert_ids_to_tokens(input_ids.tolist()[0])
+    actual_tokens = actual_tokens[1:-1]
+
+    id = 0
+    word_encodings = []
+    while id < len(actual_tokens):
+        curr_word = [actual_tokens[id]]
+        curr_enc = [sub_word_encodings[id]]
+        if id < len(actual_tokens) - 1:
+            while actual_tokens[id + 1][0] == "#":
+                id += 1
+                curr_word.append(actual_tokens[id])
+                curr_enc.append(sub_word_encodings[id])
+                if id == len(actual_tokens) - 1:
+                    break
+        actual_enc = np.mean(curr_enc, keepdims=1)
+        actual_enc = actual_enc[0]
+        id += 1
+
+        word_encodings.append(actual_enc)
+
+    text_enc = torch.stack(word_encodings)
 
     assert len(text_enc) == len(text)
 
     return text_enc
+
 
 def encode_text(tokenizer, model,elan_file, hdf5_file_name):
     """
@@ -60,10 +116,10 @@ def encode_text(tokenizer, model,elan_file, hdf5_file_name):
                 text.append(word)
 
     # Split text into short enough parts
-    numb_parts = len(text) // 500 + 1
+    numb_parts = len(text) // 400 + 1
     full_text_enc = []
     for part in range(numb_parts):
-        text_part= text[part*500:(part+1)*500]
+        text_part = text[part*400:(part+1)*400]
 
         # Encode the current part
         text_part_enc = text_to_feat(tokenizer, model, text_part)
@@ -100,7 +156,7 @@ if __name__ == "__main__":
     tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-german-cased')
     model = DistilBertModel.from_pretrained('distilbert-base-german-cased')
 
-    curr_folder = "/home/tarask/Documents/Datasets/SaGa/All_the_transcripts/"
+    curr_folder = "/home/tarask/Documents/Datasets/SaGa/Raw/All_the_transcripts/"
 
     # go though the gesture features
     for item in os.listdir(curr_folder):
