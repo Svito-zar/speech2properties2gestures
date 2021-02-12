28f44a5758e07112801cb85aecfa02f7b93cee8a
Taras Kucherenko <tarask@kth.se>
2021-02-12 12:10:07 +0100

    Add sample with no "class"

--- my_code/predict_ges_properites/create_dataset.py
+++ my_code/predict_ges_properites/create_dataset.py
@@ -53,6 +53,29 @@ def create_dataset(general_folder, specific_subfolder, feature_name, dataset_nam
                 curr_feat_timing = curr_feat_vec[:2].round(1)
                 curr_feat_values = curr_feat_vec[2:]
 
+                # add some sample with no semantic gesture
+                if feat_id > 0:
+
+                    for time_st in np.arange(prev_feat_timing[1] + 0.5, prev_feat_timing[1] + 1.5, 0.4):
+
+                        if time_st >= curr_feat_timing[0]:
+                            break
+
+                        # Save some extra info which might be useful later on
+                        output_vector = np.concatenate(([recording_id, time_st.round(1)], [0 for _ in range(5)]))
+
+                        # find the corresponding words
+                        curr_word_id = bisect.bisect(text_timing[:, 0], time_st)
+
+                        # encode current word with the next three and previous three words
+                        # while also storing time offset from the current time-step
+                        input_vector = [
+                            np.concatenate(([text_timing[word_id, 0].round(1) - time_st.round(1)], text_array[word_id, 2:]))
+                            for word_id in range(curr_word_id - 3, curr_word_id + 4)]
+
+                        X_dataset.append(np.array(input_vector))
+                        Y_dataset.append(output_vector)
+
                 for time_st in np.arange(curr_feat_timing[0], curr_feat_timing[1], 0.1):
                     # Save some extra info which might be useful later on
                     output_vector = np.concatenate(([recording_id, time_st.round(1)], curr_feat_values))
@@ -80,6 +103,8 @@ def create_dataset(general_folder, specific_subfolder, feature_name, dataset_nam
                         X_dataset.append(np.array(input_vector))
                         Y_dataset.append(output_vector)
 
+                prev_feat_timing = curr_feat_timing
+
             print(np.array(Y_dataset).shape)
             print(np.array(X_dataset).shape)
 
@@ -90,7 +115,7 @@ def create_dataset(general_folder, specific_subfolder, feature_name, dataset_nam
 if __name__ == "__main__":
 
     gen_folder = "/home/tarask/Documents/Datasets/SaGa/Processed/feat/"
-    dataset_name = subfolder = "test"
+    dataset_name = subfolder = "val"
     feature_name = "R.G.Right Semantic"
 
     create_dataset(gen_folder, subfolder, feature_name, dataset_name)
