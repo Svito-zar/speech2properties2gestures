3b00a3b6bc22869919bd24f47aba40abe25ad27f
Taras Kucherenko <tarask@kth.se>
2021-03-09 15:46:34 +0100

    Moved to StratifiedKFold

--- my_code/predict_ges_properites/trainer.py
+++ my_code/predict_ges_properites/trainer.py
@@ -8,7 +8,7 @@ from my_code.predict_ges_properites.GestPropDataset import GesturePropDataset
 from my_code.misc.shared import RANDOM_SEED
 from pytorch_lightning import Trainer, seed_everything
 
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold as KFold
 
 seed_everything(RANDOM_SEED)
 
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     print('--------------------------------')
 
     # K-fold Cross Validation model evaluation
-    for fold, (train_ids, test_ids) in enumerate(kfold.split(train_n_val_dataset)):
+    for fold, (train_ids, test_ids) in enumerate(kfold.split(train_n_val_dataset.x_dataset, train_n_val_dataset.y_labels)):
         # Print
         print(f'FOLD {fold}')
         print('--------------------------------')
