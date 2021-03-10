b3ccf7270546493758d11c073d04662a14678c41
Taras Kucherenko <tarask@kth.se>
2021-03-10 17:18:45 +0100

    New way of processing the dataset Now consider each and every time frame but not only those where properties are present

--- my_code/data_processing/annotations/create_dataset.py
+++ my_code/data_processing/annotations/create_dataset.py
@@ -4,7 +4,7 @@ import os
 import bisect
 
 
-def create_dataset(general_folder, specific_subfolder, feature_name, dataset_name):
+def create_dataset(general_folder, specific_subfolder, feature_name, dataset_name, feature_dim):
     """
 
     Args:
@@ -12,6 +12,7 @@ def create_dataset(general_folder, specific_subfolder, feature_name, dataset_nam
         specific_subfolder: name of a specific subfolder
         feature_name:       name of the feature we are considering
         dataset_name:       name of the dataset
+        feature_dim:        dimensionality of the feature considered
 
     Returns:
         Nothing, save dataset in npy file
@@ -46,6 +47,38 @@ def create_dataset(general_folder, specific_subfolder, feature_name, dataset_nam
             text_array = text_hf.get("text")
             text_timing = text_array[:, :2]
 
+            # Consider all the time-frames except for the first and last three words, since we need three words for the contexts
+            start_time = text_timing[3, 0].round(1)
+            end_time = text_timing[-4, 0].round(1)
+            duration = end_time - start_time
+            total_number_of_frames = int(duration * 5) # 0.2s time-steps
+
+            """ print("Start time: ", start_time)
+            print("End time: ", end_time)
+            print("Total numb of frames: ", total_number_of_frames) """
+
+            # First save all the text features
+            curr_file_X_data = np.zeros((total_number_of_frames, 7, 769))
+
+            time_ind = 0
+            for time_st in np.arange(start_time, end_time-0.1, 0.2):
+
+                # find the corresponding words
+                curr_word_id = bisect.bisect(text_timing[:, 0], time_st)
+
+                # encode current word with the next three and previous three words
+                # while also storing time offset from the current time-step
+                input_vector = [
+                    np.concatenate(([text_timing[word_id, 0].round(1) - time_st.round(1)], text_array[word_id, 2:]))
+                    for word_id in range(curr_word_id - 3, curr_word_id + 4)]
+
+                curr_file_X_data[time_ind] = np.array(input_vector)
+
+                time_ind = int( (time_st.round(1) - start_time) * 5 )
+
+            # Create dataset for Y features
+            curr_file_Y_data = np.zeros((total_number_of_frames, feature_dim+2))
+
             for feat_id in range(spec_feat.shape[0]):
 
                 curr_feat_vec = spec_feat[feat_id]
@@ -54,76 +87,89 @@ def create_dataset(general_folder, specific_subfolder, feature_name, dataset_nam
                 curr_feat_timing = curr_feat_vec[:2].round(1)
                 curr_feat_values = curr_feat_vec[2:]
 
-                # add some sample with no semantic gesture
-                if feat_id > 0:
-
-                    for time_st in np.arange(prev_feat_timing[1] + 0.5, prev_feat_timing[1] + 1.5, 0.4):
-
-                        if time_st >= curr_feat_timing[0]:
-                            break
-
-                        # Save some extra info which might be useful later on
-                        output_vector = np.concatenate(([recording_id, time_st.round(1)], [0 for _ in range(5)]))
-
-                        # find the corresponding words
-                        curr_word_id = bisect.bisect(text_timing[:, 0], time_st)
-
-                        # encode current word with the next three and previous three words
-                        # while also storing time offset from the current time-step
-                        input_vector = [
-                            np.concatenate(([text_timing[word_id, 0].round(1) - time_st.round(1)], text_array[word_id, 2:]))
-                            for word_id in range(curr_word_id - 3, curr_word_id + 4)]
-
-                        X_dataset.append(np.array(input_vector))
-                        Y_dataset.append(output_vector)
-
                 for time_st in np.arange(curr_feat_timing[0], curr_feat_timing[1], 0.2):
 
                     # Save some extra info which might be useful later on
                     output_vector = np.concatenate(([recording_id, time_st.round(1)], curr_feat_values))
 
-                    # find the corresponding words
-                    curr_word_id = bisect.bisect(text_timing[:, 0], time_st)
+                    time_ind = int((time_st.round(1) - start_time) * 5)
 
-                    if curr_word_id + 4 > len(text_array):
-                        continue
+                    curr_file_Y_data[time_ind] = output_vector
 
-                    # encode current word with the next three and previous three words
-                    # while also storing time offset from the current time-step
-                    input_vector = [
-                        np.concatenate(([text_timing[word_id, 0].round(1) - time_st.round(1)], text_array[word_id, 2:]))
-                        for word_id in range(curr_word_id - 3, curr_word_id + 4)]
+            if len(X_dataset) == 0:
+                X_dataset = curr_file_X_data
+                Y_dataset = curr_file_Y_data
+            else:
+                X_dataset = np.concatenate((X_dataset, curr_file_X_data))
+                Y_dataset = np.concatenate((Y_dataset, curr_file_Y_data))
 
-                    if output_vector[2] == 1:
-                        mulp_factor = 30
-                    elif output_vector[3] == 1:
-                        mulp_factor = 4
-                    elif output_vector[5] == 1:
-                        mulp_factor = 3
-                    elif output_vector[6] == 1:
-                        mulp_factor = 2
-                    else:
-                        mulp_factor = 1
-
-                    for _ in range(mulp_factor):
-                        X_dataset.append(np.array(input_vector))
-                        Y_dataset.append(output_vector)
-
-                prev_feat_timing = curr_feat_timing
-
+            print(np.asarray(X_dataset, dtype=np.float32).shape)
             print(np.asarray(Y_dataset, dtype=np.float32).shape)
-            print(np.array(X_dataset).shape)
 
     # create dataset file
     Y = np.asarray(Y_dataset, dtype=np.float32)
     X = np.asarray(X_dataset, dtype=np.float32)
-    np.save(gen_folder + dataset_name+ "_Y_" + feature_name + ".npy", Y)
-    np.save(gen_folder + dataset_name + "_X_" + feature_name + ".npy", X)
+
+    # upsample underrepresented classes
+    print("Upsampling ...")
+    X_ups, Y_ups = upsample(X, Y, feature_dim)
+
+    # save files
+    np.save(gen_folder + dataset_name+ "_Y_" + feature_name + ".npy", Y_ups)
+    np.save(gen_folder + dataset_name + "_X_" + feature_name + ".npy", X_ups)
+
+
+def upsample(X, Y, feature_length):
+    """
+
+    Args:
+        X:                  input dataset
+        Y:                  output dataset
+        feature_length:     number of features in the dataset
+
+    Returns:
+        X_upsampled:        upsampled input dataset with equalized features frequencies
+        Y_upsampled:        upsampled output dataset with equalized features frequencies
+
+    """
+
+    freq = np.zeros(feature_length)
+    for feat in range(feature_length):
+        column = Y[:, 2 + feat]
+        freq[feat] = np.sum(column)
+
+    max_freq = np.max(freq)
+    multipliers = [int(2 * max_freq // freq[feat]) for feat in range(feature_length)]
+
+    Y_upsampled = list(np.copy(Y))
+    X_upsampled = list(np.copy(X))
+
+    for ind in range(Y.shape[0]):
+        multipl_factor = 1
+        for feat in range(feature_length):
+            if Y[ind, 2 + feat] == 1:
+                multipl_factor = max(multipl_factor, multipliers[feat])
+        if multipl_factor > 1:
+            for _ in range(multipl_factor-1):
+                Y_upsampled.append(Y[ind])
+                X_upsampled.append(X[ind])
+
+    X_upsampled = np.asarray(X_upsampled, dtype=np.float32)
+    Y_upsampled = np.asarray(Y_upsampled, dtype=np.float32)
+
+    freq = np.zeros(feature_length)
+    for feat in range(feature_length):
+        column = Y_upsampled[:, 2 + feat]
+        freq[feat] = np.sum(column)
+
+    return X_upsampled, Y_upsampled
+
 
 if __name__ == "__main__":
 
     gen_folder = "/home/tarask/Documents/Datasets/SaGa/Processed/feat/"
-    dataset_name = subfolder = "train"
-    feature_name = "R.G.Left.Phase"
+    dataset_name = subfolder = "train_n_val"
+    feature_name = "R.S.Semantic Feature"
+    feature_dim = 8
 
-    create_dataset(gen_folder, subfolder, feature_name, dataset_name)
+    create_dataset(gen_folder, subfolder, feature_name, dataset_name, feature_dim)
