f87a356b13a62c11221e74723a5722359342184e
tarask <tarask@kth.se>
2021-04-07 09:38:38 +0200

    Save best hparams for Audio2Prop

--- my_code/predict_ges_properites/hparams_search/hparams_range_of_values.py
+++ my_code/predict_ges_properites/hparams_search/hparams_range_of_values.py
@@ -1,18 +1,18 @@
 def hparam_options(hparams, trial):
 
-    hparams.CNN["hidden_dim"] = trial.suggest_categorical("hidden_dim", [16, 32, 64])
+    hparams.CNN["hidden_dim"] = trial.suggest_categorical("hidden_dim", [32, 64, 96, 128])
 
     hparams.CNN["n_layers"] = trial.suggest_categorical("n_layers", [1, 2, 3])
     
-    hparams.CNN["kernel_size"] = trial.suggest_categorical("kernel_size", [3, 5, 7])
+    hparams.CNN["kernel_size"] = trial.suggest_categorical("kernel_size", [3, 5, 7, 9, 11])
 
-    hparams.CNN["dropout"] = trial.suggest_uniform("dropout", 0.2, 0.5)
+    hparams.CNN["dropout"] = trial.suggest_uniform("dropout", 0.1, 0.5)
     
-    hparams.Loss["beta"] = trial.suggest_uniform("beta", 0.9, 0.99)
+    hparams.Loss["beta"] = trial.suggest_uniform("beta", 0.8, 0.92)
     
-    hparams.Loss["gamma"] = trial.suggest_int("gamma", 4, 10)
+    hparams.Loss["gamma"] = trial.suggest_int("gamma", 2, 9)
     
-    hparams.Loss["alpha"] = trial.suggest_uniform("alpha", 0.8, 0.99)
+    hparams.Loss["alpha"] = trial.suggest_uniform("alpha", 0.62, 0.85)
 
     hparams.Optim["name"] = trial.suggest_categorical(
         "optim_name", ["adam", "sgd", "rmsprop"]
@@ -30,8 +30,7 @@ def hparam_options(hparams, trial):
         "Schedule_step_size", 1, 10
     )
 
-    hparams.Optim["Schedule"]["warm_up"] = trial.suggest_int("lr_warm_up", 300, 2000)
-
     hparams.lr = trial.suggest_loguniform("lr", 1e-5, 5e-3)
+    hparams.batch_size = trial.suggest_categorical("batch_size", [64, 128, 256, 512])
 
     return hparams
