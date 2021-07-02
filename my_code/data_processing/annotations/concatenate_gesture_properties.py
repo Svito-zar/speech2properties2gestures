bd683839d9e0a99a3f384b15d19d6b1a45782fbf
nagyrajmund <rajmund977@gmail.com>
2021-07-02 15:20:13 +0200

    Add quick 'n' dirty scripts for gestExist and AllProp arrays

---
+++ my_code/data_processing/annotations/concatenate_gesture_properties.py
@@ -0,0 +1,29 @@
+import numpy as np
+from os.path import join
+
+dataset_dir = "../../../dataset/processed/numpy_arrays/train_n_val"
+
+get_dataset = lambda fname : np.load(join(dataset_dir, fname + ".npy"))
+
+dataset_names = ["Phrase_properties", "Phase_properties", "Semantic_properties", "Audio", "Text"]
+datasets = { name : get_dataset(name) for name in dataset_names }
+
+# Make sure that all lengths are equal
+lengths = [len(dataset) for dataset in datasets.values()]
+assert len(np.unique(lengths)) == 1
+
+# Make sure that the timestamps match
+timestamps_per_array = [dataset[:, :2] for idx, (name, dataset) in enumerate(datasets.items()) if "properties" in name]
+timestamps_per_array = np.stack(timestamps_per_array)
+assert len(np.unique(timestamps_per_array, axis=0)) == 1
+
+timestamps = datasets["Phase_properties"][:, :2]
+property_features = np.concatenate(
+    [dataset[:, 2:] for name, dataset in datasets.items() if "properties" in name],
+    axis = 1
+)
+
+joint_dataset = np.concatenate((timestamps, property_features), axis=1)
+
+print("FINAL SHAPE:", joint_dataset.shape)
+np.save(join(dataset_dir, "All_properties.npy"), joint_dataset)
---
+++ my_code/data_processing/annotations/create_gesture_existance_array.py
@@ -0,0 +1,46 @@
+import numpy as np
+from os.path import join
+
+from torch.utils import data
+
+def print_stats(dataset):
+    no_feat = 0
+    has_phase = 0
+    has_phrase = 0
+    has_semantic = 0
+    for feat in dataset:
+        if np.sum(feat[2:]) == 0:
+            no_feat += 1
+        
+        if np.sum(feat[2:6]) > 0:
+            has_phrase += 1
+        
+        if np.sum(feat[6:11]) > 0:
+            has_phase += 1
+
+        if np.sum(feat[11:]) > 0:
+            has_semantic += 1
+
+    print(f"Phase: {has_phase}, Phrase: {has_phrase}, Semantic: {has_semantic}, No feat: {no_feat}")
+
+
+dataset_dir = "../../../dataset/processed/numpy_arrays/train_n_val"
+
+property_dataset = np.load(join(dataset_dir, "All_properties.npy"))
+
+print_stats(property_dataset)
+
+gesture_existance_array = np.zeros((property_dataset.shape[0], 3))
+# Copy timestamps
+gesture_existance_array[:, :2] = property_dataset[:, :2]
+
+# Set binary value for gesture existance
+for ind, feat in enumerate(property_dataset):
+    gesture_existance_array[ind, 2] = np.max(feat[2:])
+
+assert gesture_existance_array[:, 2].max() == 1 
+assert gesture_existance_array[:, 2].min() == 0
+
+np.save(join(dataset_dir, "Gest_exist_properties.npy"), gesture_existance_array)
+print("FINAL SHAPE:", gesture_existance_array.shape)
+print("# gestures:", gesture_existance_array[:, 2].sum())
