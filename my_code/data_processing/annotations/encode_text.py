30c2061c85816eeeea87ed8072338b4aabb329e8
Taras Kucherenko <tarask@kth.se>
2021-09-01 10:14:38 +0200

    Implemented FastText

--- my_code/data_processing/annotations/encode_text.py
+++ my_code/data_processing/annotations/encode_text.py
@@ -1,6 +1,8 @@
 from tqdm import tqdm
 from os.path import join
 from transformers import DistilBertTokenizer, DistilBertModel
+import fasttext
+import fasttext.util
 import torch
 import pympi
 import numpy as np
@@ -35,7 +37,7 @@ def clean_str(word):
     return final_word
 
 
-def text_to_feat(tokenizer, model, text):
+def BERT_text_to_feat(tokenizer, model, text):
     """
     Encode given text into BERT feature vector
     Args:
@@ -82,14 +84,35 @@ def text_to_feat(tokenizer, model, text):
     return text_enc
 
 
-def encode_text(tokenizer, model, elan_object, hdf5_dataset):
+def FastText_text_to_feat(model, text):
+    """
+    Encode given text into FastText feature vector
+    Args:
+        model:       FastText model itself
+        text:        text to be encoded
+
+    Returns:
+        text_enc     resulting feature vector
+
+    """
+
+    # Encode the whole text
+    text_enc = [model[clean_str(str(word))] for word in text]
+
+    assert len(text_enc) == len(text)
+
+    return torch.Tensor(text_enc)
+
+
+def encode_text(elan_object, hdf5_dataset, language_model, tokenizer=None, model=None ):
     """
     Encode features of a current file and save into hdf5 dataset
     Args:
-        tokenizer:            BERT tokenizer
-        model:                BERT model itself
         elan_object:          ELAN object containing the annotations
         hdf5_dataset:         the hdf5 dataset where the vectors will be saved
+        language_model:       NLP model to use, such as BERT or FastText
+        tokenizer:            BERT tokenizer
+        model:                BERT model itself
 
     Returns:
         nothing, but the text features are saved into 'hdf5_dataset'
@@ -117,7 +140,12 @@ def encode_text(tokenizer, model, elan_object, hdf5_dataset):
         text_part = text[part*400:(part+1)*400]
 
         # Encode the current part
-        text_part_enc = text_to_feat(tokenizer, model, text_part)
+        if language_model == "BERT":
+            text_part_enc = BERT_text_to_feat(tokenizer, model, text_part)
+        elif language_model == "FastText":
+            text_part_enc = FastText_text_to_feat(model, text_part)
+        else:
+            raise NotImplementedError("The language model ", language_model, " is not implemented yet!")
 
         full_text_enc.append(text_part_enc)
 
@@ -143,12 +171,11 @@ def encode_text(tokenizer, model, elan_object, hdf5_dataset):
     hdf5_dataset.create_dataset(name="text", data=curr_column_features.astype(np.float64))
 
 
-def create_hdf5_file(annotation_filename):
+def create_hdf5_file(annotation_filename, target_dir):
     """
     Create the output hdf5 object based on the ELAN filename.
     """
     file_idx = annotation_filename[:2]
-    target_dir = "../../../dataset/processed/word_vectors/train_n_val/"
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir)
     hdf5_file_name = join(target_dir, f"{file_idx}_text.hdf5")
@@ -159,11 +186,25 @@ def create_hdf5_file(annotation_filename):
 
 if __name__ == "__main__":
 
-    # create DistilBERT model
-    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-german-cased')
-    model = DistilBertModel.from_pretrained('distilbert-base-german-cased')
+    annotation_folder = "/home/tarask/Documents/Datasets/SaGa/Processed/Aug/transcripts/"
 
-    annotation_folder = "../../../dataset/transcripts/"
+    text_folder ="/home/tarask/Documents/Datasets/SaGa/Processed/Aug/processed/word_vectors/train_n_val/"
+
+    NLP_model = "FastText"
+    word_embedding_dim = 300
+
+    if NLP_model == "BERT":
+        # create DistilBERT model
+        tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-german-cased')
+        model = DistilBertModel.from_pretrained('distilbert-base-german-cased')
+    elif NLP_model == "FastText":
+        print("Loading FastText model ...")
+        # Load FastText model
+        tokenizer = None
+        fasttext.util.download_model('de', if_exists='ignore')  # German
+        model = fasttext.load_model(f"cc.de.{word_embedding_dim}.bin")
+    else:
+        raise NotImplementedError("The language model " + NLP_model + " is not implemented yet!")
 
     # go though the gesture features
     for filename in tqdm(os.listdir(annotation_folder)):
@@ -173,8 +214,8 @@ if __name__ == "__main__":
         annotation_file = join(annotation_folder, filename)
         elan_object = pympi.Elan.Eaf(file_path=annotation_file)
         
-        hdf5_dataset = create_hdf5_file(filename)
+        hdf5_dataset = create_hdf5_file(filename, text_folder)
         
-        encode_text(tokenizer, model, elan_object, hdf5_dataset)
+        encode_text(elan_object, hdf5_dataset, NLP_model, tokenizer, model )
 
         hdf5_dataset.close()
